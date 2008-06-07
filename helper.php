bdc9b0c20344469f5c1e5ccfea264168c2c53070
Michael Klier <chi@chimeric.de>
2008-06-07 23:04:26 +0200

    fixed code style
    darcs-hash:20080607210426-23886-213b686426dfcc53a66230e50705444de147fe73.gz

--- helper.php
+++ helper.php
@@ -8,160 +8,158 @@
 if (!defined('DOKU_INC')) die();
 
 class helper_plugin_editor extends DokuWiki_Plugin {
-    
-  function getInfo(){
-    return array(
-      'author' => 'Gina Häußge, Michael Klier, Esther Brunner',
-      'email'  => 'dokuwiki@chimeric.de',
-      'date'   => '2008-02-13',
-      'name'   => 'Editor Plugin (helper class)',
-      'desc'   => 'Returns pages recently edited by a given user',
-      'url'    => 'http://wiki.splitbrain.org/plugin:editor',
-    );
-  }
-  
-  function getMethods(){
-    $result = array();
-    $result[] = array(
-      'name'   => 'getEditor',
-      'desc'   => 'returns pages recently edited by a given user',
-      'params' => array(
-        'namespace (optional)' => 'string',
-        'number (optional)' => 'integer',
-        'user (required)' => 'string'),
-      'return' => array('pages' => 'array'),
-    );
-    return $result;
-  }
 
-  /**
-   * Get pages edited by user from a given namespace
-   */
-  function getEditor($ns = '', $num = NULL, $user = ''){
-    global $conf;
-  
-    if (!$user) $user = $_REQUEST['user'];
-    
-    $first  = $_REQUEST['first'];
-    if (!is_numeric($first)) $first = 0;
-    
-    if ((!$num) || (!is_numeric($num))) $num = $conf['recent'];
-    
-    if ($user == '@ALL'){                                                 // all users
-      $type = 'all';
-    } elseif ($user{0} == '@'){                                           // filter group
-      global $auth;
-      
-      if (($auth) && ($auth->cando['getUsers'])){
-        $user = $auth->retrieveUsers(0, 0, array('grps' => substr($user, 1)));
-        $user = array_keys($user);
-        $type = 'group';
-      } else {
-        msg('Group filtering not supported by authentification class.', -1);
-        return array();
-      }
-    } elseif (preg_match("/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/", $user)){ // filter IP
-      $type = 'ip';
-    } else {                                                              // filter user
-      $type = 'user';
+    function getInfo() {
+        return array(
+                'author' => 'Gina Häußge, Michael Klier, Esther Brunner',
+                'email'  => 'dokuwiki@chimeric.de',
+                'date'   => '2008-02-13',
+                'name'   => 'Editor Plugin (helper class)',
+                'desc'   => 'Returns pages recently edited by a given user',
+                'url'    => 'http://wiki.splitbrain.org/plugin:editor',
+                );
     }
-      
-    // read all recent changes. (kept short)
-    $lines = file($conf['changelog']);
-  
-    // handle lines
-    $result = array();
-    $count  = 0;
-    for ($i = count($lines)-1; $i >= 0; $i--){
-      $rec = $this->_handleRecent($lines[$i], $ns, $type, $user);
-      if ($rec !== false){
-        if (--$first >= 0) continue; // skip first entries
-        $result[] = $rec;
-        $count++;
-        // break when we have enough entries
-        if ($count >= $num) break;
-      }
+
+    function getMethods() {
+        $result = array();
+        $result[] = array(
+                'name'   => 'getEditor',
+                'desc'   => 'returns pages recently edited by a given user',
+                'params' => array(
+                    'namespace (optional)' => 'string',
+                    'number (optional)' => 'integer',
+                    'user (required)' => 'string'),
+                'return' => array('pages' => 'array'),
+                );
+        return $result;
     }
-    
-    // clear static $seen in _handleRecent
-    $this->_handleRecent(array(), '', 'clear', '');
-              
-    return $result;
-  }
-    
-/* ---------- Changelog function adapted for the Editor Plugin ---------- */
-    
-  /**
-   * Internal function used by $this->getPages()
-   *
-   * don't call directly
-   *
-   * @see getRecents()
-   * @author Andreas Gohr <andi@splitbrain.org>
-   * @author Ben Coburn <btcoburn@silicodon.net>
-   * @author Esther Brunner <wikidesign@gmail.com>
-   */
-  function _handleRecent($line, $ns, $type, $user){
-    global $auth;                          // authentification class
-    static $seen = array();                // caches seen pages and skip them
-    
-    if ($type == 'clear') $seen = array(); // clear seen pages cache
-    if (empty($line)) return false;        // skip empty lines
-  
-    // split the line into parts
-    $recent = parseChangelogLine($line);
-    if ($recent === false) return false;
-  
-    // skip seen ones
-    if(isset($seen[$recent['id']])) return false;
-    
-    // entry clauses for user and ip filtering
-    switch ($type){
-    case 'all':
-      break;
-    case 'user':
-      if ($recent['user'] != $user) return false;
-      break;
-    case 'group':
-      if (!in_array($recent['user'], $user)) return false;
-      break;
-    case 'ip':
-      if ($recent['ip'] != $user) return false;
-      break;
+
+    /**
+     * Get pages edited by user from a given namespace
+     */
+    function getEditor($ns = '', $num = NULL, $user = '') {
+        global $conf;
+
+        if (!$user) $user = $_REQUEST['user'];
+
+        $first  = $_REQUEST['first'];
+        if (!is_numeric($first)) $first = 0;
+
+        if ((!$num) || (!is_numeric($num))) $num = $conf['recent'];
+
+        if ($user == '@ALL') {                                                 // all users
+            $type = 'all';
+        } elseif ($user{0} == '@') {                                           // filter group
+            global $auth;
+
+            if (($auth) && ($auth->cando['getUsers'])) {
+                $user = $auth->retrieveUsers(0, 0, array('grps' => substr($user, 1)));
+                $user = array_keys($user);
+                $type = 'group';
+            } else {
+                msg('Group filtering not supported by authentification class.', -1);
+                return array();
+            }
+        } elseif (preg_match("/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/", $user)) { // filter IP
+            $type = 'ip';
+        } else {                                                              // filter user
+            $type = 'user';
+        }
+
+        // read all recent changes. (kept short)
+        $lines = file($conf['changelog']);
+
+        // handle lines
+        $result = array();
+        $count  = 0;
+        for ($i = count($lines)-1; $i >= 0; $i--) {
+            $rec = $this->_handleRecent($lines[$i], $ns, $type, $user);
+            if ($rec !== false) {
+                if (--$first >= 0) continue; // skip first entries
+                $result[] = $rec;
+                $count++;
+                // break when we have enough entries
+                if ($count >= $num) break;
+            }
+        }
+
+        // clear static $seen in _handleRecent
+        $this->_handleRecent(array(), '', 'clear', '');
+
+        return $result;
     }
-  
-    // skip minors
-    if ($recent['type']==='e') return false;
-  
-    // remember in seen to skip additional sights
-    $seen[$recent['id']] = 1;
-  
-    // check if it's a hidden page
-    if (isHiddenPage($recent['id'])) return false;
-  
-    // filter namespace
-    if (($ns) && (strpos($recent['id'], $ns.':') !== 0)) return false;
-    
-    // check ACL
-    $recent['perm'] = auth_quickaclcheck($recent['id']);
-    if ($recent['perm'] < AUTH_READ) return false;
-  
-    // check existance
-    $recent['file']   = wikiFN($recent['id']);
-    $recent['exists'] = @file_exists($recent['file']);
-    if (!$recent['exists']) return false;
-    
-    $recent['desc']   = $recent['sum'];
-    if ($recent['user']){
-      $userinfo = $auth->getUserData($recent['user']);
-      if ($userinfo) $recent['user'] = $userinfo['name'];
-    } else {
-      $recent['user'] = $recent['ip'];
+
+    /* ---------- Changelog function adapted for the Editor Plugin ---------- */
+
+    /**
+     * Internal function used by $this->getPages()
+     *
+     * don't call directly
+     *
+     * @see getRecents()
+     * @author Andreas Gohr <andi@splitbrain.org>
+     * @author Ben Coburn <btcoburn@silicodon.net>
+     * @author Esther Brunner <wikidesign@gmail.com>
+     */
+    function _handleRecent($line, $ns, $type, $user) {
+        global $auth;                          // authentification class
+        static $seen = array();                // caches seen pages and skip them
+
+        if ($type == 'clear') $seen = array(); // clear seen pages cache
+        if (empty($line)) return false;        // skip empty lines
+
+        // split the line into parts
+        $recent = parseChangelogLine($line);
+        if ($recent === false) return false;
+
+        // skip seen ones
+        if(isset($seen[$recent['id']])) return false;
+
+        // entry clauses for user and ip filtering
+        switch ($type) {
+            case 'all':
+                break;
+            case 'user':
+                if ($recent['user'] != $user) return false;
+                break;
+            case 'group':
+                if (!in_array($recent['user'], $user)) return false;
+                break;
+            case 'ip':
+                if ($recent['ip'] != $user) return false;
+                break;
+        }
+
+        // skip minors
+        if ($recent['type']==='e') return false;
+
+        // remember in seen to skip additional sights
+        $seen[$recent['id']] = 1;
+
+        // check if it's a hidden page
+        if (isHiddenPage($recent['id'])) return false;
+
+        // filter namespace
+        if (($ns) && (strpos($recent['id'], $ns.':') !== 0)) return false;
+
+        // check ACL
+        $recent['perm'] = auth_quickaclcheck($recent['id']);
+        if ($recent['perm'] < AUTH_READ) return false;
+
+        // check existance
+        $recent['file']   = wikiFN($recent['id']);
+        $recent['exists'] = @file_exists($recent['file']);
+        if (!$recent['exists']) return false;
+
+        $recent['desc']   = $recent['sum'];
+        if ($recent['user']) {
+            $userinfo = $auth->getUserData($recent['user']);
+            if ($userinfo) $recent['user'] = $userinfo['name'];
+        } else {
+            $recent['user'] = $recent['ip'];
+        }
+
+        return $recent;
     }
-    
-    return $recent;
-  }
-    
 }
-  
-//Setup VIM: ex: et ts=4 enc=utf-8 :
+//vim:ts=4:sw=4:et:enc=utf-8:
--- syntax.php
+++ syntax.php
@@ -17,96 +17,97 @@ require_once(DOKU_PLUGIN.'syntax.php');
 
 class syntax_plugin_editor extends DokuWiki_Syntax_Plugin {
 
-  function getInfo(){
-    return array(
-      'author' => 'Gina Häußge, Michael Klier, Esther Brunner',
-      'email'  => 'dokuwiki@chimeric.de',
-      'date'   => '2008-02-13',
-      'name'   => 'Editor Plugin',
-      'desc'   => 'Displays a list of recently changed wiki pages by a given author',
-      'url'    => 'http://wiki.splitbrain.org/plugin:editor',
-    );
-  }
+	function getInfo() {
+		return array(
+				'author' => 'Gina Häußge, Michael Klier, Esther Brunner',
+				'email'  => 'dokuwiki@chimeric.de',
+				'date'   => '2008-02-13',
+				'name'   => 'Editor Plugin',
+				'desc'   => 'Displays a list of recently changed wiki pages by a given author',
+				'url'    => 'http://wiki.splitbrain.org/plugin:editor',
+				);
+	}
 
-  function getType(){ return 'substition'; }
-  function getPType(){ return 'block'; }
-  function getSort(){ return 309; }
-  
-  function connectTo($mode){
-    $this->Lexer->addSpecialPattern('\{\{editor>.+?\}\}',$mode,'plugin_editor');
-  }
+	function getType() { return 'substition'; }
+	function getPType() { return 'block'; }
+	function getSort() { return 309; }
 
-  function handle($match, $state, $pos, &$handler){
-    global $ID;
-    
-    $match = substr($match, 9, -2); // strip {{editor> from start and }} from end
-    list($match, $flags) = explode('&', $match, 2);
-    $flags = explode('&', $flags);
-    list($match, $refine) = explode(' ', $match, 2);
-    list($ns, $user) = explode('?', $match);
-    
-    if (!$user){
-      $user = $ns;
-      $ns   = '';
-    }
-    
-    if (($ns == '*') || ($ns == ':')) $ns = '';
-    elseif ($ns == '.') $ns = getNS($ID);
-    else $ns = cleanID($ns);
-    
-    return array($ns, trim($user), $flags, $refine);
-  }
+	function connectTo($mode) {
+		$this->Lexer->addSpecialPattern('\{\{editor>.+?\}\}',$mode,'plugin_editor');
+	}
 
-  function render($mode, &$renderer, $data) {
-    list($ns, $user, $flags, $refine) = $data;
-    
-    if ($user == '@USER@') $user = $_SERVER['REMOTE_USER'];
-        
-    if ($my =& plugin_load('helper', 'editor')) $pages = $my->getEditor($ns, '', $user);
-    
-    // use tag refinements?
-    if ($refine){
-      if (plugin_isdisabled('tag') || (!$tag = plugin_load('helper', 'tag'))){
-        msg('The Tag Plugin must be installed to use tag refinements.', -1);
-      } else {
-        $pages = $tag->tagRefine($pages, $refine);
-      }
-    }
-    
-    if (!$pages) return true; // nothing to display
-    
-    if ($mode == 'xhtml'){
+	function handle($match, $state, $pos, &$handler) {
+		global $ID;
 
-      // prevent caching to ensure content is always fresh
-      $renderer->info['cache'] = false;
-      
-      // let Pagelist Plugin do the work for us
-      if (plugin_isdisabled('include')
-        || (!$pagelist =& plugin_load('helper', 'pagelist'))){
-        msg('The Pagelist Plugin must be installed for editor lists to work.', -1);
-        return false;
-      }
-      
-      // hide user column, unless for groups
-      if ($user{0} != '@') $pagelist->column['user'] = false;
-      
-      $pagelist->setFlags($flags);
-      $pagelist->startList();
-      foreach ($pages as $page){        
-        $pagelist->addPage($page);
-      }
-      $renderer->doc .= $pagelist->finishList();      
-      return true;
-            
-    // for metadata renderer
-    } elseif ($mode == 'metadata'){
-      foreach ($pages as $page){
-        $renderer->meta['relation']['references'][$page['id']] = true;
-      }
-      
-      return true;
-    }
-    return false;
-  }
-  
+		$match = substr($match, 9, -2); // strip {{editor> from start and }} from end
+		list($match, $flags) = explode('&', $match, 2);
+		$flags = explode('&', $flags);
+		list($match, $refine) = explode(' ', $match, 2);
+		list($ns, $user) = explode('?', $match);
+
+		if (!$user) {
+			$user = $ns;
+			$ns   = '';
+		}
+
+		if (($ns == '*') || ($ns == ':')) $ns = '';
+		elseif ($ns == '.') $ns = getNS($ID);
+		else $ns = cleanID($ns);
+
+		return array($ns, trim($user), $flags, $refine);
+	}
+
+	function render($mode, &$renderer, $data) {
+		list($ns, $user, $flags, $refine) = $data;
+
+		if ($user == '@USER@') $user = $_SERVER['REMOTE_USER'];
+
+		if ($my =& plugin_load('helper', 'editor')) $pages = $my->getEditor($ns, '', $user);
+
+		// use tag refinements?
+		if ($refine) {
+			if (plugin_isdisabled('tag') || (!$tag = plugin_load('helper', 'tag'))) {
+				msg('The Tag Plugin must be installed to use tag refinements.', -1);
+			} else {
+				$pages = $tag->tagRefine($pages, $refine);
+			}
+		}
+
+		if (!$pages) return true; // nothing to display
+
+		if ($mode == 'xhtml') {
+
+			// prevent caching to ensure content is always fresh
+			$renderer->info['cache'] = false;
+
+			// let Pagelist Plugin do the work for us
+			if (plugin_isdisabled('include')
+					|| (!$pagelist =& plugin_load('helper', 'pagelist'))) {
+				msg('The Pagelist Plugin must be installed for editor lists to work.', -1);
+				return false;
+			}
+
+			// hide user column, unless for groups
+			if ($user{0} != '@') $pagelist->column['user'] = false;
+
+			$pagelist->setFlags($flags);
+			$pagelist->startList();
+			foreach ($pages as $page) {        
+				$pagelist->addPage($page);
+			}
+			$renderer->doc .= $pagelist->finishList();      
+			return true;
+
+			// for metadata renderer
+		} elseif ($mode == 'metadata') {
+			foreach ($pages as $page) {
+				$renderer->meta['relation']['references'][$page['id']] = true;
+			}
+
+			return true;
+		}
+		return false;
+	}
+
 }
+//vim:ts=4:sw=4:et:enc=utf-8:
